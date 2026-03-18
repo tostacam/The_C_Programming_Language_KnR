0ca81dfb607b51cae5c6a9e03339a68a56eacb30
tostacam <edgar-tostado@hotmail.com>
2026-03-18 16:51:49 -0700

    add section 2.1

---
+++ chapter02_types_operators_and_expressions/example_02-01.c
@@ -0,0 +1,16 @@
+// 2.1 Variable Names
+
+#include <stdio.h>
+
+int main() {
+    // variable names
+    int age = 25;
+    int _count = 10; // not recommended to start with underscore
+                     // since library routines use such names
+    int total_sum = 100;
+    int value2 = 50;
+
+    // case-sensitive
+    int data = 1;
+    int Data = 2;
+} 
