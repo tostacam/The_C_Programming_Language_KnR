ec18766fe147279e552c15dda894679af84af39e
tostacam <edgar-tostado@hotmail.com>
2026-03-23 15:46:29 -0700

    add section 4.5

---
+++ chapter04_functions_and_program_structure/exercise_04-05/main.c
@@ -0,0 +1,47 @@
+// 4.5 Header Files
+
+#include <stdio.h>
+#include <stdlib.h> /* for atof() */
+#include "calc.h"
+
+#define MAXOP   100 /* max size of operand or operator */
+
+/* reverse Polish calculator */
+int main(){
+  int type;
+  double op2;
+  char s[MAXOP];
+
+  while((type = getop(s)) != EOF){
+    switch(type){
+      case NUMBER:
+        push(atof(s));
+        break;
+      case '+':
+        push(pop() + pop());
+        break;
+      case '*':
+        push(pop() * pop());
+        break;
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2 = pop();
+        if(op2 != 0.0)
+          push(pop() / op2);
+        else
+          printf("error: zero divisor\n");
+        break;
+      case '\n':
+        printf("result = %.8g\n", pop());
+        break;
+      default:
+        printf("error: unknown command %s\n", s);
+        break;
+    }
+  }
+
+  return 0;
+}
