e06218b92007c0fc4c7b0b6e449b714400227048
tostacam <edgar-tostado@hotmail.com>
2026-03-23 20:40:14 -0700

    add example 5.1

---
+++ chapter05_pointers_and_arrays/example_05-01.c
@@ -0,0 +1,16 @@
+// 5.1 Pointers and Addresses
+
+#include <stdio.h>
+
+int main(){
+  int x = 1, y = 2, z[10];
+  int *ip;    /* ip is a pointer to int */
+
+  ip = &x;    /* ip now points to x */
+  y = *ip;    /* y is now 1 */
+  *ip = 0;    /* x is now 0 */
+  ip = &z[0]; /* ip now points to z[0] */
+
+  /* parentheses needed, otherwise pointer itself would be incremented*/
+  (*ip)++;
+}
