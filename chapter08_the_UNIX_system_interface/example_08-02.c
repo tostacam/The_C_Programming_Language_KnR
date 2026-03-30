e76af7c087eb20bc4e30c8f51a4ffee00cb7bec3
tostacam <edgar-tostado@hotmail.com>
2026-03-30 16:56:31 -0700

    add section 8.2

---
+++ chapter08_the_UNIX_system_interface/example_08-02.c
@@ -0,0 +1,38 @@
+// 8.2 Low Level I/O-Read and Write
+
+//#include "syscalls.h"
+#include <unistd.h>
+#include <stdio.h>
+
+int getchar1(void), getchar2(void);
+
+/* copy input to output */
+int main(){
+  char buf[BUFSIZ];
+  int n;
+
+  while((n = read(0, buf, BUFSIZ)) > 0)
+    write(1, buf, n);
+  
+  return 0;
+}
+
+/* getchar: unbuffered single character input */
+int getchar1(void){
+  char c;
+
+  return (read(0, &c, 1) == 1) ? (unsigned char)c : EOF;
+}
+
+/* getchar: simple buffered version */
+int getchar2(void){
+  static char buf[BUFSIZ];
+  static char *bufp = buf;
+  static int n = 0;
+
+  if(n == 0){ /* buffer is empty */
+    n = read(0, buf, sizeof buf);
+    bufp = buf;
+  }
+  return (--n >= 0) ? (unsigned char)*bufp++ : EOF;
+}
