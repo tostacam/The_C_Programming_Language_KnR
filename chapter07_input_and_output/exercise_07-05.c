4ae205078e0035e38155a10597c445cfd427edbd
tostacam <edgar-tostado@hotmail.com>
2026-03-29 23:21:54 -0700

    add exercise 7.5

---
+++ chapter07_input_and_output/exercise_07-05.c
@@ -0,0 +1,89 @@
+// Exercise 7.5
+// Rewrite the postfix calculator of Chapter 4 to use scanf and/or 
+// sscanf to do the input and number conversion.
+
+#include <stdio.h>
+#include <stdlib.h> /* for atof() */
+#include <ctype.h>
+
+#define MAXOP   100 /* max size of operand or operator */
+#define NUMBER '0'  /* signal that a number was found */
+#define MAXVAL  100 /* maximum depth of val stack */
+
+int sp = 0;         /* next free stack position */
+double val[MAXVAL]; /* value stack */
+
+int getop(char []);
+void push(double);
+double pop(void);
+
+/* reverse Polish calculator */
+int main(){
+  int type;
+  double op2;
+  char s[MAXOP];
+
+  while((type = getop(s)) != EOF){
+    switch(type){
+      case NUMBER:
+        push(atof(s));
+        break;
+      case '+':
+        push(pop() + pop());
+        break;
+      case '*':
+        push(pop() * pop());
+        break;
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2 = pop();
+        if(op2 != 0.0)
+          push(pop() / op2);
+        else
+          printf("error: zero divisor\n");
+        break;
+      case '=':
+        printf("result = %.8g\n", pop());
+        break;
+      default:
+        printf("error: unknown command %s\n", s);
+        break;
+    }
+  }
+
+  return 0;
+}
+
+/* push: push f onto value stack */
+void push (double f){
+  if(sp < MAXVAL)
+    val[sp++] = f;
+  else
+    printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from the stack */
+double pop(void){
+  if(sp > 0)
+    return val[--sp];
+  else{
+    printf("error: stack empty\n");
+    return 0.0;
+  }
+}
+
+/* getop: get next operator or numeric operand */
+int getop(char s[]){
+  int i;
+
+  if(scanf("%s", s) != 1)
+    return EOF;
+
+  if(sscanf(s, "%lf", &(double){0}) == 1)
+    return NUMBER;
+
+  return s[0];
+}
