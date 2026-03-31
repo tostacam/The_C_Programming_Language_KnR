f725c451805e280a58f7b0bbe3d450c59076267e
tostacam <edgar-tostado@hotmail.com>
2026-03-30 23:34:10 -0700

    add section 8.6

---
+++ chapter08_the_UNIX_system_interface/example_08-06.c
@@ -0,0 +1,59 @@
+// 8.6 Example-Listing Directories
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+
+void fsize(char *);
+void dirwalk(char *, void (*fcn)(char *));
+
+/* print file sizes */
+int main(int argc, char **argv){
+  if(argc == 1)        /* default: current directory */
+    fsize(".");
+  else
+    while (--argc > 0)
+      fsize(*++argv);
+
+  return 0;
+}
+
+/* fsize: print size of file "name" */
+void fsize(char *name){
+  struct stat stbuf;
+
+  if(stat(name, &stbuf) == -1){
+    fprintf(stderr, "fsize: can't access %s\n", name);
+    return;
+  }
+
+  if((stbuf.st_mode & S_IFMT) == S_IFDIR)
+    dirwalk(name, fsize);
+
+  printf("%8lld %s\n", (long long)stbuf.st_size, name);
+}
+
+/* dirwalk: apply fcn to all files in dir */
+void dirwalk(char *dir, void (*fcn)(char *)){
+  char name[1024];
+  struct dirent *dp;
+  DIR *dfd;
+
+  if((dfd = opendir(dir)) == NULL){
+    fprintf(stderr, "dirwalk: can't open %s\n", dir);
+    return;
+  }
+
+  while((dp = readdir(dfd)) != NULL){
+    if(strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+      continue;
+
+    snprintf(name, sizeof(name), "%s/%s", dir, dp->d_name);
+    (*fcn)(name);
+  }
+
+  closedir(dfd);
+}
